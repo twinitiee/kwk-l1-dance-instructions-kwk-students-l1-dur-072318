6836144931dbf32d2309dc9eeb279a68d2a6fb79
twinitiee <twinitiee@gmail.com>
2018-07-23 20:12:51 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,37 @@
 
 # Code your methods below
+
+
+def Assume the proper stance
+  1. Plant legs far apart, bend knees slightly and keep posture loose
+end
+
+def base footwork
+  1. Lift right foot
+  2. Return right foot to the ground
+  3. Finishing with a **small skip-step** backward
+end
+
+def skip step left 
+  1. Lower left front to ground
+  2. Bounce left foot back up slightly, kicking it a few inches back
+  3. Left, Right, left, and left
+end
+
+def skip step Right
+  1. lower right foot to the ground
+  2. bounce right foot back up slightly, kicking it a few inches back
+  3. Right, left, right, and right 
+end
+
+def bob the reins
+  1. holding your arms out in front of you, straight and at the chest level
+  2. cross your right wrist over your left and hold them together
+  3. lift your arms and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times.
+end 
+
+def lasso
+  1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
+  2. Lift right arm up and point right elbow diagonally to the right.
+  3 Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso
+end
