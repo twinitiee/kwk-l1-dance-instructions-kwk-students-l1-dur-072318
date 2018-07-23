0885c512edd4264bfb76fc4ccfb15855d47bb500
twinitiee <twinitiee@gmail.com>
2018-07-23 19:59:57 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,5 +2,16 @@
 # Code your methods below
 
 def gangnam style dance
-  Assume the proper stance
-  1 
+  Step 1. Plant legs far apart, bend knees slightly and keep posture loose
+  Step 2. Lift right follow_redirect
+  Step 3. Return right foot to the ground
+  Step 4. Finishing with a small skip-step backward
+  Step 5. Lower left foot to the ground
+  Step 6. Bounce left foot back up slightly
+  Step 7. Left, Right, Left, and Left
+  Step 8. Lower right foot to the ground
+  Step 9. Bounce right foot back up slightly, kicking it a few inches back
+  Step 10. Right, Left, Right and Right
+  Step 11. Holding your arms out in front of you, straight and at chest level 
+  Step 12. Cross your right wrist over your left and hold them together
+  Step 13. Lift
