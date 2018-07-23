f50ee0e28a911db98aaac3bc9980086246ab5ff3
twinitiee <twinitiee@gmail.com>
2018-07-23 20:08:09 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -9,6 +9,10 @@ end
 def base footwork
   1. Lift right foot
   2. Return right foot to the ground
-
-3. Finishing with a **small skip-step** backward
+  3. Finishing with a **small skip-step** backward
 end
+
+def skip step left 
+  1. Lower left front to ground
+  2. Bounce left foot back up slightly, kicking it a few inches back
+  3. Left, Right, 
