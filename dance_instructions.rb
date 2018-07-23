e99b92c122d20e3a07061086a33ca5eedc6db307
twinitiee <twinitiee@gmail.com>
2018-07-23 20:06:56 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -6,4 +6,10 @@ def Assume the proper stance
   1. Plant legs far apart, bend knees slightly and keep posture loose
 end
 
-def 
+def base footwork
+  1. Lift right foot
+
+2. Return right foot to the ground
+
+3. Finishing with a **small skip-step** backward
+end
