dd6071c892e5897ace1e01e4060aff7c12493289
twinitiee <twinitiee@gmail.com>
2018-07-23 20:05:30 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,22 +1,4 @@
 
 # Code your methods below
 
-def gangnam style dance
-  Step 1. Plant legs far apart, bend knees slightly and keep posture loose
-  Step 2. Lift right follow_redirect
-  Step 3. Return right foot to the ground
-  Step 4. Finishing with a small skip-step backward
-  Step 5. Lower left foot to the ground
-  Step 6. Bounce left foot back up slightly
-  Step 7. Left, Right, Left, and Left
-  Step 8. Lower right foot to the ground
-  Step 9. Bounce right foot back up slightly, kicking it a few inches back
-  Step 10. Right, Left, Right and Right
-  Step 11. Holding your arms out in front of you, straight and at chest level 
-  Step 12. Cross your right wrist over your left and hold them together
-  Step 13. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song, eight times 
-  Step 14. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left
-  Step 15. Lift your right arm up and point right elbow diagonally to the right
-  Step 16. Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso, eight times
-  Step 17. Be cool, be snazzy
-end 
+def 
